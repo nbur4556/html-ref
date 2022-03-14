d151af92a84b6088d066a63bfdd771f1b1140589
nbur4556 <nbur4556@gmail.com>
2022-03-14 13:29:12 -0500

    feat: create page for tag details

---
+++ lib/detailpage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Detailpage extends StatelessWidget {
+  Detailpage({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello Page'),
+    );
+  }
+}
