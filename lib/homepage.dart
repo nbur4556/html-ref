78fb5929c71d422aa789f3c2d4b1c2490c07af22
nbur4556 <nbur4556@gmail.com>
2022-03-13 22:28:54 -0500

    feat: create grid of outline button elements

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,9 +6,28 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HTML Reference!'),
-      ),
+      body: TagGrid(),
+    );
+  }
+}
+
+class TagGrid extends StatelessWidget {
+  TagGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      children: [
+        OutlinedButton(
+          onPressed: () => print('test'),
+          child: Text('<Tag>'),
+        ),
+        OutlinedButton(
+          onPressed: () => print('test'),
+          child: Text('<Tag>'),
+        )
+      ],
     );
   }
 }
