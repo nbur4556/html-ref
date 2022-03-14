71c40e829dad5673b1b9a5d278336422b50b3721
nbur4556 <nbur4556@gmail.com>
2022-03-13 22:18:44 -0500

    init: new flutter application

---
+++ android/app/src/main/kotlin/com/example/html_ref/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.html_ref
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('HTML Refference'),
+      ),
+    );
+  }
+}
