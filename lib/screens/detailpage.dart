66eaa74bfab6257294bb6e7765220bf4e87443dc
nbur4556 <nbur4556@gmail.com>
2022-03-17 20:32:32 -0500

    feat: create detail content and section widgets

--- lib/screens/detailpage.dart
+++ lib/screens/detailpage.dart
@@ -15,9 +15,34 @@ class Detailpage extends StatelessWidget {
         title: Text('<$tagName>'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('<$tagName>'),
-      ),
+      body: DetailContent(),
+    );
+  }
+}
+
+class DetailContent extends StatelessWidget {
+  DetailContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        DetailTextSection(),
+        DetailTextSection(),
+      ],
+    );
+  }
+}
+
+class DetailTextSection extends StatelessWidget {
+  DetailTextSection({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30),
+      child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
     );
   }
 }
