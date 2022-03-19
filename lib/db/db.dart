efaee79abcc5f0a20b5212326d86e0cb63c42a2f
nbur4556 <nbur4556@gmail.com>
2022-03-19 10:31:52 -0500

    feat: create tables for html ref model

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -3,8 +3,12 @@ import 'package:path/path.dart';
 
 class Database {
   String dbName = 'html_ref_tags.db';
+  int versionNum = 1;
 
   dynamic open() async {
-    return openDatabase(join(await getDatabasesPath(), dbName));
+    return openDatabase(join(await getDatabasesPath(), dbName),
+        onCreate: (db, version) => db.execute(
+            'CREATE TABLE ref_tag(id INTEGER PRIMARY KEY, tag_name TEXT, description TEXT, attributes TEXT)'),
+        version: versionNum);
   }
 }
